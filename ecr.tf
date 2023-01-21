0d6db87b5fe7dd1caa0ea02518e411887b273214
cagatayakk <cagatayakkiran3757@gmail.com>
2023-01-21 19:21:13 +0000

    update

--- ecr.tf
+++ ecr.tf
@@ -21,7 +21,30 @@ resource "aws_ecr_repository" "devopsyolujenkins" {
   }
 }
 
+resource "aws_iam_role" "ec2ecrfullaccess" {
+  name = "ecr_ec2_permission"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ecr-ec2_profile"
+  role = aws_iam_role.ec2ecrfullaccess.name
+}
 
 output "registry_id" {
   description = "The account ID of the registry holding the repository."
